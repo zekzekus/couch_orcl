d1fbce239fc4fa3f7826e43cd2d7550cbf360ae9
Zekeriya KOÇ <zekzekus@gmail.com>
2010-12-02 13:20:58 +0200

    an example file added

--- test.sql
+++ test.sql
@@ -1,31 +1,24 @@
-create or replace procedure cdb_test_sql is
+﻿create or replace procedure cdb_test is
   conn           cdb_connection;
-  dlist          cdb_sql.t_doc_list;
+  doc1           cdb_document;
 begin
   -- static methods to create a test database
-  --cdb_connection.delete_db('http://admin:admin@10.81.3.221:5984/', 'orcl001');
-  --cdb_connection.create_db('http://admin:admin@10.81.3.221:5984/', 'orcl001');
+  cdb_connection.delete_db('http://admin:admin@10.81.3.221:5984/', 'orcl001');
+  cdb_connection.create_db('http://admin:admin@10.81.3.221:5984/', 'orcl001');
 
   -- create connection to a specific database
   conn        :=
     cdb_connection(
       host        => '10.81.3.221',
       port        => 5984,
-      db_name     => 'orcl002',
+      db_name     => 'orcl001',
       username    => 'admin',
       password    => 'admin');
   conn.print();
-
-  cdb_sql.
-   sql_to_doc(
-    'urun',
-    'select * from bba_to_aymm_urun@aymm where rownum < 31',
-    dlist);
-
-  for i in dlist.first .. dlist.last loop
-    dlist(i).conn := conn;
-    dlist(i).set_id(dlist(i).get('barcode').get_number());
-    dlist(i).save();
-  end loop;
+  
+  doc1 := cdb_document(conn);
+  doc1.put('name', 'zekeriya');
+  doc1.put('age', 27);
+  doc1.save();
 end;
 /
---
+++ test_sql.sql
@@ -0,0 +1,31 @@
+create or replace procedure cdb_test_sql is
+  conn           cdb_connection;
+  dlist          cdb_sql.t_doc_list;
+begin
+  -- static methods to create a test database
+  --cdb_connection.delete_db('http://admin:admin@10.81.3.221:5984/', 'orcl001');
+  --cdb_connection.create_db('http://admin:admin@10.81.3.221:5984/', 'orcl001');
+
+  -- create connection to a specific database
+  conn        :=
+    cdb_connection(
+      host        => '10.81.3.221',
+      port        => 5984,
+      db_name     => 'orcl001',
+      username    => 'admin',
+      password    => 'admin');
+  conn.print();
+
+  cdb_sql.
+   sql_to_doc(
+    'urun',
+    'select * from bba_to_aymm_urun@aymm where rownum < 31',
+    dlist);
+
+  for i in dlist.first .. dlist.last loop
+    dlist(i).conn := conn;
+    dlist(i).set_id(dlist(i).get('barcode').get_number());
+    dlist(i).save();
+  end loop;
+end;
+/
