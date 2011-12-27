85841c4d61f21971d48205cdf677f6c046c98fca
Zekeriya C <zekzekus@gmail.com>
2011-12-27 10:56:48 +0200

    install and uninstall scripts added for couch_orcl

---
+++ install.sql
@@ -0,0 +1,31 @@
+﻿  /*
+      This file is part of couch_orcl.
+
+      couch_orcl is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      couch_orcl is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with couch_orcl.  If not, see <http://www.gnu.org/licenses/>.
+  */
+  
+PROMPT -----------------------------------;
+PROMPT -- Compiling objects for COUCH_ORCL --;
+PROMPT -----------------------------------;
+@@uninstall.sql
+@@CDB_UTL.pks
+@@CDB_UTL.pkb
+@@CDB_CONNECTION.tps
+@@CDB_CONNECTION.tpb
+@@CDB_DOCUMENT.tps
+@@CDB_DOCUMENT.tpb
+@@CDB_SQL.pks
+@@CDB_SQL.pkb
+@@test.sql
+@@test_sql.sql
---
+++ uninstall.sql
@@ -0,0 +1,27 @@
+﻿
+  /*
+      This file is part of couch_orcl.
+
+      couch_orcl is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      couch_orcl is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with couch_orcl.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+declare begin
+  begin execute immediate 'drop procedure cdb_test_sql'; exception when others then null; end;
+  begin execute immediate 'drop procedure cdb_test'; exception when others then null; end;
+  begin execute immediate 'drop package cdb_sql'; exception when others then null; end;
+  begin execute immediate 'drop type cdb_document'; exception when others then null; end;
+  begin execute immediate 'drop type cdb_connection'; exception when others then null; end;
+  begin execute immediate 'drop package cdb_utl'; exception when others then null; end;
+end;
+/
